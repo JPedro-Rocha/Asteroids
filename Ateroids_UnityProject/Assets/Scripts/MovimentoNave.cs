38946e5940f772ad4a5837afb466bd6c7d9cc9f9
JPedro-Rocha <jp.oliveirarocha@hotmail.com>
2020-09-24 22:01:05 -0300

    adicionar tiro e asteroides

--- Ateroids_UnityProject/Assets/Scripts/MovimentoNave.cs
+++ Ateroids_UnityProject/Assets/Scripts/MovimentoNave.cs
@@ -8,26 +8,54 @@ public class MovimentoNave : MonoBehaviour
     [SerializeField] float movespeed = 1f;
     [SerializeField] float rotationspeed = 100f;
 
+    public AudioClip crash;
+    public AudioClip shoot;
+
+    public GameObject bullet;
+    [SerializeField] public Transform firePoint;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetButtonDown("Fire1")) { Shoot(); }//atirar
     }
 
     void FixedUpdate()
     {
-        rb.transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationspeed * Time.fixedDeltaTime);
-        rb.AddForce(transform.up * movespeed * Input.GetAxis("Vertical"));
+        rb.transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationspeed * Time.fixedDeltaTime);//rotação
+        rb.AddForce(transform.up * movespeed * Input.GetAxis("Vertical"));//movimento vertical
+
+        //if (Input.GetButtonDown("Fire1")) { Shoot(); }//atirar
+
         /*if (Input.GetAxis("Vertical") == 1)
         {
             rb.transform.position += transform.up * Time.deltaTime * movespeed;
         }
         */
+    }
+
+    void ShootBullet()
+    {
+        // spawna bullet
+        Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+
+        // som de tiro
+        AudioSource.PlayClipAtPoint(shoot, Camera.main.transform.position);
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        AudioSource.PlayClipAtPoint(shoot, Camera.main.transform.position);
     }
 }
